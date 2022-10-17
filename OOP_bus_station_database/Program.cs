0e477e32dcba1ee37f873bc193c4ab6feb89a5d4
Максим Корман <mkorman359@gmail.com>
2022-10-17 15:36:29 +0300

    Add project files.

---
+++ OOP_bus_station_database/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_bus_station_database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
